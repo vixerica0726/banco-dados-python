1ae404f26a974a4260fb550225ff802b06eeea28
Erica Felix De Oliveira <vixerica0726@gmail.com>
2025-02-22 20:27:10 -0300

    Add files via upload
    Trabalhando com banco de dados e python.

---
+++ alterar_tabela.py
@@ -0,0 +1,15 @@
+﻿import sqlite3  # Importando o módulo sqlite3
+
+conexao = sqlite3.connect('teste')
+cursor = conexao.cursor() # Criando um cursor
+
+# Alterar nome da tabela
+# cursor.execute('ALTER TABLE usuarios RENAME TO usuario') # Renomeando a tabela
+# cursor.execute('ALTER TABLE usuario ADD COLUNM telefone INT') # Adicionando uma coluna
+# cursor.execute('ALTER TABLE usuario DROP COLUMN telefone') # Deletando uma coluna
+cursor.execute('ALTER TABLE usuario RENAME COLUMN COLUNM TO telefone_contato') # Renomeando uma coluna
+
+conexao.commit() # Salvando as alterações
+conexao.close() # Fechando a conexão
+
+print('Conexão realizada com sucesso!')
---
+++ excluir_tabela.py
@@ -0,0 +1,14 @@
+﻿# Teste conectando banco de dados com python.
+
+import sqlite3  # Importando o módulo sqlite3
+
+conexao = sqlite3.connect('teste')
+cursor = conexao.cursor() # Criando um cursor
+
+# cursor.execute('CREATE TABLE produtos(id INT, nome VARCHAR(100), endereco VARCHAR(100), email VARCHAR(100)); ')
+cursor.execute('DROP TABLE produtos') # Deletando a tabela definitivamente
+
+conexao.commit() # Salvando as alterações
+conexao.close() # Fechando a conexão
+
+print('Conexão realizada com sucesso!')
