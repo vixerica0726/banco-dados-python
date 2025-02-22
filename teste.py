26ff403eefd6443810ae90a79bf6e6cea1050051
Erica Felix <vixerica0726@gmail.com>
2025-02-22 19:44:18 -0300

    teste

---
+++ teste.py
@@ -0,0 +1,13 @@
+﻿# Teste conectando banco de dados com python.
+
+import sqlite3  # Importando o módulo sqlite3
+
+conexao = sqlite3.connect('teste')
+cursor = conexao.cursor() # Criando um cursor
+
+cursor.execute('')
+
+conexao.commit() # Salvando as alterações
+conexao.close() # Fechando a conexão
+
+print('Conexão realizada com sucesso!')
